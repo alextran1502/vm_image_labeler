f8dc0747c53bf3768daaa19d3ee89744de6e58e1
Alex Tran <alex.tran1502@gmail.com>
2019-12-08 20:51:10 -0600

    Completed canvas that can draw and remove circle with R key

---
+++ src/components/Rectangle.js
@@ -0,0 +1,156 @@
+var EventEmitter = require('events');
+
+class Rectangle extends EventEmitter {
+    constructor(canvas, f) {
+        super()
+        this.canvas = canvas;
+        this.className = 'Rectangle';
+        this.isDrawing = false;
+        this.origX = 0;
+        this.origY = 0;
+        this.activeObj = null;
+        this.bindEvents();
+        this.f = f;
+        this.currentObject = null;
+        this.rectNumb = 0;
+        this.hoverTarget = false;
+        document.addEventListener('keydown', this.handleKey.bind(this), false)
+    }
+
+    bindEvents() {
+        this.canvas.on('mouse:down', (o) => {
+            this.onMouseDown(o);
+        });
+        this.canvas.on('mouse:move', (o) => {
+            this.onMouseMove(o);
+        });
+        this.canvas.on('mouse:up', (o) => {
+            this.onMouseUp(o);
+        });
+        this.canvas.on('object:moving', (o) => {
+            this.disable();
+        })
+
+        this.canvas.on('mouse:over', (e) => {
+            if (e.target.lockMovementX) {
+                this.hoverTarget = true;
+                this.currentObject = e.target;
+                e.target.set('fill', 'red');
+                e.target.opacity = 0.3;
+                this.canvas.renderAll();
+            } else {
+                return
+            }
+
+        })
+
+        // this.canvas.on('mouse:dblclick', e => {
+        //     this.emit('remove', e.target.name)
+        //     this.canvas.remove(e.target)
+
+        // })
+
+        this.canvas.on('mouse:out', (e) => {
+            e.target.set('fill', 'transparent');
+            this.canvas.renderAll();   
+            this.hoverTarget = false
+        });
+
+    }
+
+    handleKey(key) {
+        if (key.key == "r" && this.hoverTarget) {
+            
+            this.canvas.remove(this.currentObject)
+            this.emit('remove', this.currentObject.name)
+        } else {
+        }
+    }
+    onMouseUp(o) {
+        this.disable();
+        let drawnRect = this.activeObj
+        this.activeObj.lockMovementX = true
+        this.activeObj.lockMovementY = true
+
+        if (drawnRect.height > 25 && drawnRect.width > 25) {
+            this.emit('done', {
+                height: drawnRect.height,
+                width: drawnRect.width,
+                top: drawnRect.top,
+                left: drawnRect.left,
+                name: this.activeObj.name
+            });
+        }
+  
+
+    }
+
+    onMouseDown(o) {
+        this.enable();
+
+        var pointer = this.canvas.getPointer(o.e);
+        this.origX = pointer.x;
+        this.origY = pointer.y;
+        var rect = new fabric.Rect({
+            left: this.origX,
+            top: this.origY,
+            originX: 'left',
+            originY: 'top',
+            width: pointer.x - this.origX,
+            height: pointer.y - this.origY,
+            angle: 0,
+            transparentCorners: false,
+            hasBorders: false,
+            hasControls: false,
+            name: `rect_${this.rectNumb}`
+        });
+
+        this.canvas.add(rect).setActiveObject(rect);
+        this.rectNumb++;
+    }
+
+    onMouseMove(o) {
+
+        if (!this.isEnable()) { return; }
+        var pointer = this.canvas.getPointer(o.e);
+        this.activeObj = this.canvas.getActiveObject();
+
+        this.activeObj.stroke = 'red';
+        this.activeObj.strokeWidth = 2;
+        this.activeObj.fill = 'transparent';
+
+        if (this.origX > pointer.x) {
+            this.activeObj.set({ left: Math.abs(pointer.x) });
+        }
+        if (this.origY > pointer.y) {
+            this.activeObj.set({ top: Math.abs(pointer.y) });
+        }
+
+        let width = Math.abs(this.origX - pointer.x);
+        let height = Math.abs(this.origY - pointer.y)
+
+        if (width > 25 && height > 25) {
+            this.activeObj.set({ width: width });
+            this.activeObj.set({ height: height });
+            this.activeObj.setCoords();
+            this.canvas.renderAll();
+        } else {
+            return
+        }
+
+    }
+    isEnable() {
+        return this.isDrawing;
+    }
+
+    enable() {
+        this.isDrawing = true;
+    }
+
+    disable() {
+        this.isDrawing = false;
+    }
+
+}
+
+export default Rectangle;
